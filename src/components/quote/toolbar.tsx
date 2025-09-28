eb4cdeeb01a63029a335713f8a0af069d8375ecf
Akın CAYMAZ <akincymz@gmail.com>
2025-09-28 19:23:28 +0000

    Try fixing this error: `Runtime Error: Error: DialogClose is not defined

--- src/components/quote/toolbar.tsx
+++ src/components/quote/toolbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogDescription, DialogFooter } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogDescription, DialogFooter, DialogClose } from "@/components/ui/dialog";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useToast } from "@/hooks/use-toast";
 import { Quote, CompanyProfile, Customer, companyProfileSchema, customerSchema } from "@/lib/schema";
